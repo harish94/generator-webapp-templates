f2a34900cd31e2e7f8a600dfdb85e6ba600269ff
harish94 <atm@mintpaisa.com>
2015-06-29 15:13:57 +0530

    installation changes
    Run commands after installation

--- generators/app/index.js
+++ generators/app/index.js
@@ -62,10 +62,9 @@ var WebappTemplateGenerator = yeoman.generators.Base.extend({
    
             done();
         }.bind(this));
+  },
 
-    },
-
-    initializing: function () {
+  initializing: function () {
       this.pkg = this.templatePath('_package.json');
       
       // all templates should end with "/"
@@ -80,12 +79,9 @@ var WebappTemplateGenerator = yeoman.generators.Base.extend({
       this.appimg = 'app/assets/img';
       this.appscripts = 'app/assets/scripts';
       this.appviews = 'app/views';
-      
-      
+  },
 
-    },
-
-    scaffoldFolders: function(){
+  scaffoldFolders: function(){
      
       mkdirp(this.distpath);
       mkdirp(this.appscss);      
@@ -94,10 +90,10 @@ var WebappTemplateGenerator = yeoman.generators.Base.extend({
       mkdirp(this.appscripts);
       mkdirp(this.appviews);
       mkdirp(this.distpath);
-    },
+  },
 
 
-    writing: {
+  writing: {
       gulpfile: function () {
         this.fs.copyTpl(
           this.templatePath('_gulp.js'),
@@ -126,86 +122,84 @@ var WebappTemplateGenerator = yeoman.generators.Base.extend({
         this.fs.copy(this.templatePath(this.templateconfig+'gitattributes'),this.destinationPath('.gitattributes'));
       },
       bower: function () {
-      var bowerJson = {
-        name: _s.slugify(this.appname),
-        private: true,
-        dependencies: {}
-      };
-
-      if (this.includeBootstrap) {
-        if (this.includeSass) {
-          bowerJson.dependencies['bootstrap-sass'] = '~3.3.5';
-          bowerJson.overrides = {
-            'bootstrap-sass': {
-              'main': [
-                'assets/stylesheets/_bootstrap.scss',
-                'assets/fonts/bootstrap/*',
-                'assets/javascripts/bootstrap.js'
-              ]
-            }
-          };
-        } else {
-          bowerJson.dependencies['bootstrap'] = '~3.3.5';
-          bowerJson.overrides = {
-            'bootstrap': {
-              'main': [
-                'less/bootstrap.less',
-                'dist/css/bootstrap.css',
-                'dist/js/bootstrap.js',
-                'dist/fonts/*'
-              ]
-            }
-          };
+        var bowerJson = {
+          name: _s.slugify(this.appname),
+          private: true,
+          dependencies: {}
         }
-      } else if (this.includeJQuery) {
-        bowerJson.dependencies['jquery'] = '~2.1.1';
-      }
 
-      if (this.includeModernizr) {
-        bowerJson.dependencies['modernizr'] = '~2.8.1';
-      }
-
-      this.fs.writeJSON('bower.json', bowerJson);
-      this.fs.copy(
-        this.templatePath(this.templateconfig+'bowerrc'),
-        this.destinationPath('.bowerrc')
-      );
-    },
-
-    editorConfig: function () {
-      this.fs.copy(
-        this.templatePath(this.templateconfig+'editorconfig'),
-        this.destinationPath('.editorconfig')
-      );
-    },
-
-    h5bp: function () {
-      this.fs.copy(
-        this.templatePath(this.templateconfig+'favicon.ico'),
-        this.destinationPath('app/favicon.ico')
-      );
-
-      this.fs.copy(
-        this.templatePath(this.templateconfig+'apple-touch-icon.png'),
-        this.destinationPath('app/apple-touch-icon.png')
-      );
-
-      this.fs.copy(
-        this.templatePath(this.templateconfig+'robots.txt'),
-        this.destinationPath('app/robots.txt'));
-    },
-    styles: function () {
-      var css = 'main.scss';
-      this.fs.copyTpl(
-        this.templatePath(this.templatescss),
-        this.destinationPath( this.appscss),
-        {
-          //includeBootstrap: this.includeBootstrap
+        if (this.includeBootstrap) {
+          if (this.includeSass) {
+            bowerJson.dependencies['bootstrap-sass'] = '~3.3.5';
+            bowerJson.overrides = {
+              'bootstrap-sass': {
+                'main': [
+                  'assets/stylesheets/_bootstrap.scss',
+                  'assets/fonts/bootstrap/*',
+                  'assets/javascripts/bootstrap.js'
+                ]
+              }
+            };
+          } else {
+            bowerJson.dependencies['bootstrap'] = '~3.3.5';
+            bowerJson.overrides = {
+              'bootstrap': {
+                'main': [
+                  'less/bootstrap.less',
+                  'dist/css/bootstrap.css',
+                  'dist/js/bootstrap.js',
+                  'dist/fonts/*'
+                ]
+              }
+            };
+          }
+        } else if (this.includeJQuery) {
+          bowerJson.dependencies['jquery'] = '~2.1.1';
         }
-      );
-    },
 
-    html: function () {
+        if (this.includeModernizr) {
+          bowerJson.dependencies['modernizr'] = '~2.8.1';
+        }
+
+        this.fs.writeJSON('bower.json', bowerJson);
+        this.fs.copy(
+          this.templatePath(this.templateconfig+'bowerrc'),
+          this.destinationPath('.bowerrc')
+        );
+      },  
+      editorConfig: function () {
+        this.fs.copy(
+          this.templatePath(this.templateconfig+'editorconfig'),
+          this.destinationPath('.editorconfig')
+        );
+      },
+      h5bp: function () {
+        this.fs.copy(
+          this.templatePath(this.templateconfig+'favicon.ico'),
+          this.destinationPath('app/favicon.ico')
+        );
+
+        this.fs.copy(
+          this.templatePath(this.templateconfig+'apple-touch-icon.png'),
+          this.destinationPath('app/apple-touch-icon.png')
+        );
+
+        this.fs.copy(
+          this.templatePath(this.templateconfig+'robots.txt'),
+          this.destinationPath('app/robots.txt')
+        );
+      },
+      styles: function () {
+        var css = 'main.scss';
+        this.fs.copyTpl(
+          this.templatePath(this.templatescss),
+          this.destinationPath( this.appscss),
+          {
+            //includeBootstrap: this.includeBootstrap
+          }
+        );
+      },
+      html: function () {
       var bsPath;
       // path prefix for Bootstrap JS files
       if (this.includeBootstrap) {
@@ -217,12 +211,54 @@ var WebappTemplateGenerator = yeoman.generators.Base.extend({
         }
       }
       this.fs.copyTpl(this.templatePath('views/_index.html'),this.destinationPath('app/views/index.html'),
-      {appname: this.appName}
+        {appname: this.appName}
       );
       
-    },
+    }
+    
+  },
 
-  }  
+  install: function () {
+    this.installDependencies({
+      skipMessage: this.options['skip-install-message'],
+      skipInstall: this.options['skip-install']
+    });
+  },
+
+  end: function () {
+    var bowerJson = this.fs.readJSON(this.destinationPath('bower.json'));
+    var howToInstall =
+      '\nAfter running ' +
+      chalk.yellow.bold('npm install & bower install') +
+      ', inject your' +
+      '\nfront end dependencies by running ' +
+      chalk.yellow.bold('gulp wiredep') +
+      '.';
+
+    if (this.options['skip-install']) {
+      this.log(howToInstall);
+      return;
+    }
+
+    // wire Bower packages to .html
+    wiredep({
+      bowerJson: bowerJson,
+      directory: 'bower_components',
+      exclude: ['bootstrap-sass', 'bootstrap.js'],
+      ignorePath: /^(\.\.\/)*\.\./,
+      src: 'app/views/index.html'
+    });
+
+    if (this.includeSass) {
+      // wire Bower packages to .scss
+      wiredep({
+        bowerJson: bowerJson,
+        directory: 'bower_components',
+        ignorePath: /^(\.\.\/)+/,
+        src: 'app/styles/*.scss'
+      });
+    }
+  }
     
 });
 
