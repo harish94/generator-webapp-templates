b253b64d81cbbb1ea643e84ec97c2f498ec80919
harish94 <atm@mintpaisa.com>
2015-06-30 10:59:47 +0530

    More changes

--- generators/app/templates/_gulpfile.js
+++ generators/app/templates/_gulpfile.js
@@ -1,9 +1,11 @@
 var gulp = require('gulp'),
   webserver = require('gulp-webserver'),
+  connect = require('gulp-connect'),
   watch = require('gulp-watch'),
   less = require('gulp-less'),
   coffee = require('gulp-coffee');
- 
+  sass = require('gulp-sass');
+
 gulp.task('webserver', function() {
    gulp.src('app')
       .pipe(webserver({
@@ -12,28 +14,35 @@ gulp.task('webserver', function() {
         open: true
     }));
 });
- 
+
 gulp.task('livereload', function() {
   gulp.src(['.tmp/styles/*.css', '.tmp/scripts/*.js'])
-    .pipe(watch())
+    .pipe(watch(['.tmp/styles/*.css', '.tmp/scripts/*.js']))
     .pipe(connect.reload());
 });
- 
+
+
+gulp.task('sass', function () {
+  gulp.src('./app/assets/**/scss/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('css'));
+});
+
 gulp.task('less', function() {
   gulp.src('styles/main.less')
     .pipe(less())
     .pipe(gulp.dest('.tmp/styles'));
 });
- 
+
 gulp.task('coffee', function() {
   gulp.src('scripts/*.coffee')
     .pipe(coffee())
     .pipe(gulp.dest('.tmp/scripts'));
 });
- 
+
 gulp.task('watch', function() {
   gulp.watch('styles/*.less', ['less']);
   gulp.watch('scripts/*.coffee', ['coffee']);
 })
- 
-gulp.task('default', ['less', 'coffee', 'webserver', 'livereload', 'watch']);
+
+gulp.task('default', ['less','sass', 'coffee', 'webserver', 'livereload', 'watch']);
